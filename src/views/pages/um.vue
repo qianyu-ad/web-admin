1ab9fc4f57b9d45f94d466f86c49eb07d6d517b5
qianyu <webzhongwang@gmail.com>
2018-09-26 16:41:34 +0800

    bug fix

--- src/views/pages/um.vue
+++ src/views/pages/um.vue
@@ -183,11 +183,11 @@
             // 发布
             submit(){
                 let param = this.trim(this.cloneObj(this.param));
-                if(!param.categoryId || !param.title || !this.um.getAllHtml()){
+                if(!param.categoryId || !param.title || !UM.getEditor('myEditor').getAllHtml()){
                     this.$Message.error('请输入完整的信息');
                     return false;
                 }
-                param.content = this.um.getAllHtml();
+                param.content = UM.getEditor('myEditor').getAllHtml();
                 this.ajax({
                     type: 'post',
                     url: '/api/articles',
